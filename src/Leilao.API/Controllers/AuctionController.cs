92b236cc3b98b42ca738322b99b34fac8b4ca186
Paulo Ricardo <pr741234@gmail.com>
2024-02-12 22:14:46 -0300

    feat: Adiciona controller do Auction

--- src/Leilao.API/Controllers/AuctionController.cs
+++ src/Leilao.API/Controllers/AuctionController.cs
@@ -1,17 +1,29 @@
-﻿// Ignore Spelling: API Leilao
-
+﻿using Auction.API.UseCases.Auctions.GetCurrent;
 using Microsoft.AspNetCore.Mvc;
+using AuctionEntity = Auction.API.Entities.Auction;
 
-namespace Leilao.API.Controllers
+namespace Auction.API.Controllers
 {
   [Route("[controller]")]
   [ApiController]
   public class AuctionController : ControllerBase
   {
     [HttpGet]
+    [ProducesResponseType(typeof(AuctionEntity), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult GetCurrentAuction()
     {
-      return Ok("Paulo");
+
+      var useCase = new GetCurrentAuctionUseCase();
+
+      var result = useCase.Execute();
+
+      if (result is null)
+      {
+        return NoContent();
+      }
+
+      return Ok(result);
     }
   }
 }
