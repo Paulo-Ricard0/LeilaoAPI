e53663a792d30ef0c8505df2b16b98457531fe7f
Paulo Ricardo <pr741234@gmail.com>
2024-02-12 22:17:48 -0300

    feat: Adiciona metodo para buscar um leilao

--- src/Leilao.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/Leilao.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -1,6 +1,19 @@
-﻿namespace Leilao.API.UseCases.Auctions.GetCurrent
+﻿using Auction.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+using AuctionEntity = Auction.API.Entities.Auction;
+
+namespace Auction.API.UseCases.Auctions.GetCurrent
 {
   public class GetCurrentAuctionUseCase
   {
+    public AuctionEntity? Execute()
+    {
+      var repository = new AuctionDbContext();
+      var today = DateTime.Now;
+
+      return repository.Auctions
+        .Include(auction => auction.Items)
+        .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+    }
   }
 }
